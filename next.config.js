b5d5ec7f5f2b0072706181e7d67d35d1de94e34c
Kielo Bash Mercado <kielo_bash_mercado@dlsu.edu.ph>
2023-12-08 19:22:02 +0800

    bug 1

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  module: {
-    rules: [
-      {
-        test: /\.(wav)$/i,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "sound.wav",
-              outputPath: "assets",
-            },
-          },
-        ],
-      },
-    ],
-  },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.(wav)$/i,
+  //       use: [
+  //         {
+  //           loader: "file-loader",
+  //           options: {
+  //             name: "sound.wav",
+  //             outputPath: "assets",
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // },
 };
 
 module.exports = nextConfig;
